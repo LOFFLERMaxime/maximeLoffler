d78c68010a9d70fded0e54a155feb230731ef4c4
Maxime Loffler <mloffler@sio.jjr>
2017-03-31 11:08:44 +0200

    Ajout de la méthode Regarder(Personnage)

--- Personnage.cs
+++ Personnage.cs
@@ -2,9 +2,11 @@ public class Personnage{
 	public void Frapper(Personnage personnage, int force)
 	{
 	}
-
-	public void Regarder(Direction direction, int distance)
+	
+	public void Regarder(Direction Direction, int distance)
 	{
 	}
+
+
 }
 
